5bc299546a58d0e2d8dff5106fe15dec5a98e4dc
Mohammad Hossein Heydarchi <mhheidarchi2003@yahoo.com>
2025-12-22 11:36:48 +0100

    Fix test issues

--- app/analyzer/AnalyzerEntities.py
+++ app/analyzer/AnalyzerEntities.py
@@ -1,9 +1,9 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from PythonUtilityClasses.SystemUtility import *
 
 
 @dataclass
 class AnalyzerInfo:
-    source_file: FileInfo = FileInfo()
-    puml_file: FileInfo = FileInfo()
-    png_file: FileInfo = FileInfo()
+    source_file: FileInfo = field(default_factory=FileInfo)
+    puml_file: FileInfo = field(default_factory=FileInfo)
+    png_file: FileInfo = field(default_factory=FileInfo)
